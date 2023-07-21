dffec81cff163ec2e1fe16c37a3c5f1f43a24100
Byron Cole Lawlis <bclaw8300@gmail.com>
2023-07-21 09:11:16 -0600

    add description

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,4 +1,8 @@
 <template>
   <h1>Ignite Metal Works</h1>
   <h2>Precision metal fabrication for a flawless finish.</h2>
+  <h3>Built on Quality, Integrity, and Customer Satisfaction, Ignite Metal Works LLC has quickly grown to be Lubbock’s
+    leading welding service. Our well-rounded staff includes certified welders capable of handling any project you may
+    need. Ignite Metal Works specializes in piping, structural, and fabrication. We are systematically experienced in
+    meeting challenging safety protocols, specifications and inspection standards</h3>
 </template>
