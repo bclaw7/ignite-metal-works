bc4222346946c2eaa26e1d6c3c0a28df1c514313
Byron Cole Lawlis <bclaw8300@gmail.com>
2023-07-21 08:51:08 -0600

    add components and assets
    upload photos and logos. Start building landing page.

--- src/App.vue
+++ src/App.vue
@@ -1,25 +1,25 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
+import Home from './components/Home.vue'
 </script>
 
 <template>
   <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-    </div>
+    <img alt="Vue logo" class="logo" src="./assets/ignite-metal-works-logo.png" width="280" height="280" />
   </header>
 
   <main>
-    <TheWelcome />
+    <Home />
   </main>
 </template>
 
 <style scoped>
 header {
   line-height: 1.5;
+}
+
+main {
+  max-width: 800px;
+  margin: 0 auto;
 }
 
 .logo {
@@ -30,18 +30,7 @@ header {
 @media (min-width: 1024px) {
   header {
     display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
+    justify-content: center;
   }
 }
 </style>
---
+++ src/components/Home.vue
@@ -0,0 +1,4 @@
+<template>
+  <h1>Ignite Metal Works</h1>
+  <h2>Precision metal fabrication for a flawless finish.</h2>
+</template>
