53d31ee46d5ffc02578ffd74d87828fbb5021adc
Byron Cole Lawlis <bclaw8300@gmail.com>
2023-07-22 00:19:25 -0600

    styles and contact info

--- src/App.vue
+++ src/App.vue
@@ -5,6 +5,10 @@ import Home from './components/Home.vue'
 <template>
   <header>
     <img alt="Vue logo" class="logo" src="./assets/ignite-metal-works-logo.png" width="280" height="280" />
+    <div class="contact-info">
+      <a href="tel:8067781029" class="hover-underline-animation">&#9742; (806)778-1029</a>
+      <a href="mailto:chase@ignitemetalworks.com" class="hover-underline-animation">&#128231; ignitemetalworks.com</a>
+    </div>
   </header>
 
   <main>
@@ -15,6 +19,8 @@ import Home from './components/Home.vue'
 <style scoped>
 header {
   line-height: 1.5;
+  display: flex;
+  flex-wrap: wrap-reverse;
 }
 
 main {
@@ -24,13 +30,49 @@ main {
 
 .logo {
   display: block;
-  margin: 0 auto 2rem;
+  margin: 0 auto;
 }
 
-@media (min-width: 1024px) {
+.contact-info {
+  display: flex;
+  justify-content: space-between;
+  width: 100%;
+  padding-top: 1rem;
+}
+
+.hover-underline-animation {
+  display: inline-block;
+  position: relative;
+}
+
+.hover-underline-animation::after {
+  content: '';
+  position: absolute;
+  width: 100%;
+  transform: scaleX(0);
+  height: 2px;
+  bottom: 0;
+  left: 0;
+  background-color: var(--color-heading);
+  transform-origin: bottom right;
+  transition: transform 0.25s ease-out;
+}
+
+.hover-underline-animation:hover::after {
+  transform: scaleX(1);
+  transform-origin: bottom left;
+}
+
+@media screen and (min-width: 1024px) {
   header {
-    display: flex;
-    justify-content: center;
+    flex-wrap: nowrap;
   }
+
+  .contact-info {
+    width: auto;
+    justify-content: space-evenly;
+    flex-direction: column;
+  }
+
 }
 </style>
--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,8 +1,9 @@
 <template>
-  <h1>Ignite Metal Works</h1>
-  <h2>Precision metal fabrication for a flawless finish.</h2>
-  <h3>Built on Quality, Integrity, and Customer Satisfaction, Ignite Metal Works LLC has quickly grown to be Lubbock’s
-    leading welding service. Our well-rounded staff includes certified welders capable of handling any project you may
-    need. Ignite Metal Works specializes in piping, structural, and fabrication. We are systematically experienced in
-    meeting challenging safety protocols, specifications and inspection standards</h3>
+  <div class="slant-glass">
+    <h1>Ignite Metal Works</h1>
+    <h2>Precision metal fabrication for a flawless finish.</h2>
+    <h3 class="body-text">Ignite Metal Works specializes in piping, structural, and fabrication. We are systematically
+      experienced in
+      meeting challenging safety protocols, specifications and inspection standards</h3>
+  </div>
 </template>
